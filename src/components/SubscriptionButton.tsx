914c899045185664b442ec573a4b5c612c639e60
jrenning <jack-renning@uiowa.edu>
2023-07-08 14:16:19 -0500

    Tru ios notif, 2.

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -1,30 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function SubscriptionButton() {
-
-const Subscribe = async () => {
-    const status = await Notification.requestPermission()
-    console.log(`The status is ${status}`)
+  const Subscribe = async () => {
+    const status = await Notification.requestPermission();
+    console.log(`The status is ${status}`);
     if (status == "granted") {
-        const notif = new Notification("Hey Jack!")
+      const notif = new Notification("Hey Jack!");
+      navigator.serviceWorker.ready.then((reg) => {
+        reg.pushManager
+          .subscribe()
+          .then((pushSubscription) => {
+            console.log(pushSubscription);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+    } else {
+      console.error("Permission was weird");
+      navigator.serviceWorker.ready.then((reg) => {
+        reg.pushManager
+          .subscribe()
+          .then((pushSubscription) => {
+            console.log(pushSubscription);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
     }
-    else {
-        console.error("Permission wasn't granted")
-    }
-
-
-    // navigator.serviceWorker.ready.then(reg => {
-    //     reg.pushManager.subscribe().then((pushSubscription)=> {
-    //         console.log(pushSubscription)
-    //     }).catch((err)=> {
-    //         console.error(err)
-    //     })
-    // })
-}
+  };
 
   return (
-    <button onClick={()=> Subscribe()} className='rounded-md bg-blue-300 mb-2 mx-[200px] hover:bg-blue-100'>Subscribe</button>
-  )
+    <button
+      onClick={() => Subscribe()}
+      className="mx-[200px] mb-2 rounded-md bg-blue-300 hover:bg-blue-100"
+    >
+      Subscribe
+    </button>
+  );
 }
 
-export default SubscriptionButton
+export default SubscriptionButton;
