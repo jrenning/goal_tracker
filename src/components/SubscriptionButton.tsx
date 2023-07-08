891e9f73a791334fdd18a783a87b9f962971391f
jrenning <jack-renning@uiowa.edu>
2023-07-08 14:36:29 -0500

    Tru ios notif, 5.

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 
 function SubscriptionButton() {
+  const base64ToUint8Array = (base64: string) => {
+    const padding = "=".repeat((4 - (base64.length % 4)) % 4);
+    const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+    const rawData = window.atob(b64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
+
   const Subscribe = async () => {
     const status = await Notification.requestPermission();
     console.log(`The status is ${status}`);
     if (status == "granted") {
       const notif = new Notification("Hey Jack!");
       const options = {
-        userVisibleOnly: true
-      }
+        userVisibleOnly: true,
+        //@ts-ignore
+        applicationServerKey: base64ToUint8Array(process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY)
+      };
       navigator.serviceWorker.ready.then((reg) => {
         reg.pushManager
           .subscribe(options)
           .then((pushSubscription) => {
             console.log(pushSubscription);
-            alert(pushSubscription)
+            alert(pushSubscription);
           })
           .catch((err) => {
             console.error(err);
-            alert(err)
+            alert(err);
           });
       });
     } else {
