167bb26c4c5fd59db1d68042a58d16e0319b9def
jrenning <jack-renning@uiowa.edu>
2023-07-08 19:41:18 -0500

    Get more info.

--- src/pages/api/scheduled_notification.ts
+++ src/pages/api/scheduled_notification.ts
@@ -1,39 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { appRouter} from "~/server/api/root";
+import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 
-export default async (req: NextApiRequest, 
-    res: NextApiResponse) => {
-
-        const sendNotification = async (subscription: any) => {
-          if (subscription == null) {
-            throw new Error("The subscription was null");
-          }
-          const result = await fetch("/api/notification", {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: subscription,
-          });
-          alert(result.body);
-          alert(result.statusText);
-        };
-
-        // get the subscription data 
-
-        const caller = appRouter.createCaller({
-            prisma: prisma
-        });
-        const result = await caller.user.getCurrentUserInfo();
-
-
-        // send notification request 
-        await sendNotification(result?.subscription).then(() => res.send({message: "Success!"})).catch((err) => res.send({message: "Failed", result: result, err: err}))
-
-
-
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const sendNotification = async (subscription: any) => {
+    if (subscription == null) {
+      throw new Error("The subscription was null");
     }
+    const result = await fetch("/api/notification", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: subscription,
+    });
+    alert(result.body);
+    alert(result.statusText);
+  };
 
+  // get the subscription data
 
+  const caller = appRouter.createCaller({
+    prisma: prisma,
+  });
+  const result = await caller.user.getCurrentUserInfo();
 
+  // send notification request
+  const send = await sendNotification(result?.subscription)
+    .then(() => res.send({ message: "Success!" }))
+    .catch((err) => {
+      res.send({ message: "Failed", result: result, err: err });
+    });
+    res.send(send)
+};
