3cccef99ec1327ca49376d6df64d3e621e4094a6
jrenning <jack-renning@uiowa.edu>
2023-07-21 15:57:18 -0500

    Updated add buttons

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,23 +1,41 @@
 import Link from 'next/link';
 import React from 'react'
+import {motion} from "framer-motion"
 
 type AddButtonProps = {
     name: string
     link: string
+    number: number
 }
 
-function AddButton({name, link}: AddButtonProps) {
+function AddButton({name, link, number}: AddButtonProps) {
+
+const bottom = 13 + number*6
+let space_top = `${bottom}rem`
+let space_bottom = `${bottom-1}rem`
+
   return (
-    <div className="flex flex-row space-x-2">
-      <div className="fixed right-20 flex w-[100px] flex-wrap rounded-md bg-slate-400 p-1 text-center text-white">
+    <motion.div
+      className="flex flex-row space-x-2 justify-center items-center"
+      //animate={{ x: 20 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+        duration: 5,
+      }}
+    >
+      <div className="fixed right-28 text-xl font-semibold rounded-md bg-slate-400 p-2 text-center text-white"
+      style={{bottom: space_top}}>
         New {name}
       </div>
       <Link href={link}>
-        <button className="fixed right-8 flex h-8 w-8 items-center justify-center rounded-full bg-green-500 text-white hover:bg-green-200">
+        <button className="fixed right-8 flex h-16 w-16 items-center justify-center rounded-full bg-green-500 text-white hover:bg-green-200"
+        style={{bottom: space_bottom}}>
           +
         </button>
       </Link>
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/AddContentButton.tsx
+++ src/components/AddContentButton.tsx
@@ -5,7 +5,10 @@ function AddContentButton() {
 
 const [addActive, setAddActive] = useState(false)
 const [buttonMark, setButtonMark] = useState(true)
-const adds = [{name: "Goal", link: "/add_goal"}, {name: "Reward", link: "/add_reward"}]
+const adds = [
+  { name: "Goal", link: "/add_goal" },
+  { name: "Reward", link: "/add_reward" },
+];
 
 const handleClick = () => {
     setButtonMark(!buttonMark)
@@ -14,11 +17,11 @@ const handleClick = () => {
 
   return (
     <div className="fixed left-[63%] top-[70%]">
-      <div className="flex flex-col space-y-10 pr-12 justify-evenly">
-        {addActive ? adds.map((add) => <AddButton name={add.name} link={add.link} />) : ""}
+      <div className="flex flex-col space-y-40 pr-12 justify-evenly">
+        {addActive ? adds.map((add, index) => <AddButton name={add.name} link={add.link} number={index} />) : ""}
       </div>
       <button
-        className="fixed right-8 bottom-[7rem] flex h-12 w-12 cursor-pointer items-center
+        className="fixed right-8 bottom-[6rem] flex h-20 w-20 cursor-pointer items-center
       justify-center rounded-full bg-green-300 text-2xl font-semibold text-white shadow-md hover:bg-gray-50 hover:text-black"
       onClick={()=> handleClick()}
       >
